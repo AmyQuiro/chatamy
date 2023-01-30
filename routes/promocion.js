5814aa8a0ab692bbca1e376d3c8b5679c37a10f9
AmyQuiro <amygalaxies@gmail.com>
2023-01-30 16:22:37 -0400

    promocion unica

--- routes/promocion.js
+++ routes/promocion.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Promocion = require("../Models/Promocion");
+const Client = require("../Models/client");
 
 router.post("", (req, res) => {
     let body = req.body;
@@ -21,5 +22,31 @@ router.post("", (req, res) => {
       });
   
     })
-  })
+  });
+
+  router.get("/:id", async (req, res) => {
+    let idPromocion =   req.params.id;
+    var mongoose = require('mongoose');
+    let promocion = await Promocion.findOne({ "_id": mongoose.Types.ObjectId(idPromocion) });
+
+    console.log('promocion.grupo :>> ', promocion.grupo);
+    var selector = { "status":promocion.grupo };
+    let listaCliente= await Client.find(selector);
+    let listaCorreos="";
+    listaCliente.forEach(element => {
+     listaCorreos+=element.email+";";
+    });
+    listaCorreos= listaCorreos.substring(0,listaCorreos.length-1);
+
+    let miPromocion={
+      name:promocion.Name,
+      message:promocion.message,
+      emailList:listaCorreos
+    }
+    promocion["listaCorreos"]=listaCorreos;
+
+    res.json({ ok: true, msg: JSON.stringify(miPromocion) });
+  });
+
+
   module.exports = router;
