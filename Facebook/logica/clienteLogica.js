d8c30a73146ff02484b10c56804e2b30d452a5e1
AmyQuiro <amygalaxies@gmail.com>
2023-03-04 15:48:45 -0400

    cambio actualizar

--- Facebook/logica/clienteLogica.js
+++ Facebook/logica/clienteLogica.js
@@ -2,6 +2,7 @@ const client = require("../../Models/client");
 
 class clienteLogica {
   static async setStatus(idcliente, dbListCompras) {
+    console.info("Inicio de set status cliente");
     let filterClient = { _id: ObjectID(idcliente) };
     console.info("filterClient");
     const options = { upsert: false };
@@ -17,7 +18,13 @@ class clienteLogica {
       },
     };
 
-    const result = await client.updateOne(filterClient, updateDoc, options);
+    const result = await client.updateOne(filterClient, updateDoc);
+
+    console.log(`${result.modifiedCount} documento(s) actualizado(s)`);
+
+    const result2 = await client.updateOne(filterClient, updateDoc, options);
+
+    console.log(`${result2.modifiedCount} documento(s) 2 actualizado(s)`);
   }
 }
 module.exports = clienteLogica;
