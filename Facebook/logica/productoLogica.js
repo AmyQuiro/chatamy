1c4a43f9cc07ff45cd2782f70da30bb90b8bc83a
AmyQuiro <amygalaxies@gmail.com>
2023-03-04 11:04:57 -0400

    fix nombre producto

--- Facebook/logica/productoLogica.js
+++ Facebook/logica/productoLogica.js
@@ -1,18 +1,13 @@
-const CarritoDetalle = require('../../Models/CarritosDetalle');
-const Product = require('../../Models/Products');
+const CarritoDetalle = require("../../Models/CarritosDetalle");
+const Product = require("../../Models/Products");
 
-
- class productoLogica{
-  constructor(){
-
+class productoLogica {
+  constructor() {}
+  static async bucarProductosPorNombre(nombreProducto) {
+    var selector = { name: { $regex: nombreProducto, $options: "i" } };
+    let dbListClothes = await Product.find(selector);
+    console.log("dbListClothes :>> ", dbListClothes);
+    return dbListClothes;
   }
-  static  async bucarProductosPorNombre(nombreProducto){
-
-        var selector = { "name": { $regex: clothes, $options: "i" } };
-      let dbListClothes = await Product.find(selector);
-      console.log('dbListClothes :>> ', dbListClothes);
-return dbListClothes;
-        
-       }
 }
 module.exports = productoLogica;
